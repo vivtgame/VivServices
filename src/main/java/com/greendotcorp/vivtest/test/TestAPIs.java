3fbd025141f252d9d3d3336ded9a20c736cb418b
vivtgame <vivian.game@gmail.com>
2018-03-26 17:05:44 -0700

    Update TestAPIs.java

--- src/main/java/com/greendotcorp/vivtest/test/TestAPIs.java
+++ src/main/java/com/greendotcorp/vivtest/test/TestAPIs.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
 @Test
 @ContextConfiguration(locations = { "classpath:test-config.xml" })
